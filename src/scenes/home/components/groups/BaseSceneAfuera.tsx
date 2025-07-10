36dff9755891322a5cfa8239f959f35e7051ac83
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-10 18:36:33 -0500

    cambios realizados 21.9

--- src/scenes/home/components/groups/BaseSceneAfuera.tsx
+++ src/scenes/home/components/groups/BaseSceneAfuera.tsx
@@ -5,17 +5,18 @@
 
 
 
-// import { Borde } from "../primer/Borde";
-// import { PisoArco } from "../primer/PisoArco";
-// import { PisoCesped1 } from "../primer/PisoCesped1";
-// import { PisoCesped3 } from "../primer/PisoCesped3";
-// import { PisoLabs } from "../primer/PisoLabs";
+import { Borde } from "../primer/Borde";
+import { PisoArco } from "../primer/PisoArco";
+import { PisoCesped1 } from "../primer/PisoCesped1";
+import { PisoCesped3 } from "../primer/PisoCesped3";
+// import { PisoCesped5E } from "../primer/PisoCesped5E";
+import { PisoLabs } from "../primer/PisoLabs";
 import { PisoOctagono } from "../primer/PisoOctagono";
 import { PisoTriangulo } from "../primer/PisoTriangulo";
-// import { PisoVereda } from "../primer/pisoVereda";
-// import { PisoVereda2 } from "../primer/pisoVereda2";
+import { PisoVereda } from "../primer/pisoVereda";
+import { PisoVereda2 } from "../primer/pisoVereda2";
 import { Vereda1 } from "../primer/vereda1";
-// import { Vereda2 } from "../primer/vereda2";
+import { Vereda2 } from "../primer/vereda2";
 
 
 // cargar segun la distancia 
@@ -28,16 +29,23 @@ const BaseSceneAfuera = () => {
         <PisoOctagono />
         <Vereda1 />
         <PisoTriangulo />
-        {/* <PisoCesped1 />
+        <PisoCesped1 />
         <Vereda2 />
         <PisoArco />
         <PisoVereda />
         <PisoVereda2 />
         <PisoCesped3 />
         <Borde />
-        <PisoLabs /> */}
+        {/* <PisoCesped5E /> */}
+        {/* <PisoVereda5 /> */}
+        <PisoLabs />
       
- 
+ {/* sin fisica */}
+      {/* <Frases />
+      <Senaleticas />
+      <Cartelera />
+      <Parqueadero />
+      <Tablero /> */}
 
       </>
     
