8b48a163c86008b6d5bf40577650282abbcad067
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-10 23:12:05 -0500

    cambios realizados 24

--- src/scenes/home/components/oficina/Pared1.tsx
+++ src/scenes/home/components/oficina/Pared1.tsx
@@ -1,79 +1,47 @@
-import * as THREE from 'three';
-import { useGLTF } from '@react-three/drei';
-import { useTrimesh } from '@react-three/cannon';
-import { GLTF } from 'three-stdlib';
-import { ThreeElements } from '@react-three/fiber';
+import * as THREE from 'three'
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+import { ThreeElements } from '@react-three/fiber'
 
 type GLTFResult = GLTF & {
   nodes: {
-    Room004_1: THREE.Mesh;
-    Room004_2: THREE.Mesh;
-    Room004_3: THREE.Mesh;
-  };
+    Room004_1: THREE.Mesh
+    Room004_2: THREE.Mesh
+    Room004_3: THREE.Mesh
+  }
   materials: {
-    ['Material.094']: THREE.MeshStandardMaterial;
-    ['Material.095']: THREE.MeshStandardMaterial;
-    ['Material.066']: THREE.MeshStandardMaterial;
-  };
-};
+    ['Material.094']: THREE.MeshStandardMaterial
+    ['Material.095']: THREE.MeshStandardMaterial
+    ['Material.066']: THREE.MeshStandardMaterial
+  }
+}
 
 export function Pared1(props: ThreeElements['group']) {
   const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/oficina/pared1.glb') as unknown as GLTFResult;
 
-  const position: [number, number, number] = [72.942, 29.5, -71.785];
-
-  // Guardamos las referencias para cada colisión
-  const [ref1] = useTrimesh(() => ({
-    args: [
-      nodes.Room004_1.geometry.attributes.position.array as Float32Array,
-      nodes.Room004_1.geometry.index!.array as Uint16Array,
-    ],
-    position,
-    type: 'Static',
-  }));
-
-  const [ref2] = useTrimesh(() => ({
-    args: [
-      nodes.Room004_2.geometry.attributes.position.array as Float32Array,
-      nodes.Room004_2.geometry.index!.array as Uint16Array,
-    ],
-    position,
-    type: 'Static',
-  }));
-
-  const [ref3] = useTrimesh(() => ({
-    args: [
-      nodes.Room004_3.geometry.attributes.position.array as Float32Array,
-      nodes.Room004_3.geometry.index!.array as Uint16Array,
-    ],
-    position,
-    type: 'Static',
-  }));
-
+  
   return (
     <group {...props} dispose={null}>
-      <group name="Room004" position={position}>
+      <group name="Room004" position={[72.941, 31.953, -71.785]}>
         <mesh
-          ref={ref1}
           name="Room004_1"
           geometry={nodes.Room004_1.geometry}
           material={materials['Material.094']}
         />
         <mesh
-          ref={ref2}
           name="Room004_2"
           geometry={nodes.Room004_2.geometry}
           material={materials['Material.095']}
         />
         <mesh
-          ref={ref3}
           name="Room004_3"
           geometry={nodes.Room004_3.geometry}
           material={materials['Material.066']}
         />
       </group>
     </group>
-  );
+  )
 }
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/oficina/pared1.glb');
--- src/scenes/home/components/oficina/Pared2.tsx
+++ src/scenes/home/components/oficina/Pared2.tsx
@@ -1,62 +1,41 @@
-import * as THREE from 'three';
-import { useGLTF } from '@react-three/drei';
-import { useTrimesh } from '@react-three/cannon';
-import { GLTF } from 'three-stdlib';
-import { ThreeElements } from '@react-three/fiber';
+
+import * as THREE from 'three'
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+import { ThreeElements } from '@react-three/fiber'
 
 type GLTFResult = GLTF & {
   nodes: {
-    Plane024: THREE.Mesh;
-    Plane024_1: THREE.Mesh;
-  };
+    Plane024: THREE.Mesh
+    Plane024_1: THREE.Mesh
+  }
   materials: {
-    ['Material.066']: THREE.MeshStandardMaterial;
-    ['Material.067']: THREE.MeshStandardMaterial;
-  };
-};
-
+    ['Material.066']: THREE.MeshStandardMaterial
+    ['Material.067']: THREE.MeshStandardMaterial
+  }
+}
 export function Pared2(props: ThreeElements['group']) {
   const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/oficina/pared2.glb') as unknown as GLTFResult;
 
-  const position: [number, number, number] = [238.472, 41.7, -65.002];
-
-  // Guardar ref para colisiones
-  const [ref1] = useTrimesh(() => ({
-    args: [
-      nodes.Plane024.geometry.attributes.position.array as Float32Array,
-      nodes.Plane024.geometry.index!.array as Uint16Array,
-    ],
-    position,
-    type: 'Static',
-  }));
-
-  const [ref2] = useTrimesh(() => ({
-    args: [
-      nodes.Plane024_1.geometry.attributes.position.array as Float32Array,
-      nodes.Plane024_1.geometry.index!.array as Uint16Array,
-    ],
-    position,
-    type: 'Static',
-  }));
+  
 
   return (
     <group {...props} dispose={null}>
-      <group name="pared_vertical_1" position={position}>
+      <group name="pared_vertical_1" position={[238.472, 44.083, -65.002]}>
         <mesh
-          ref={ref1}
           name="Plane024"
           geometry={nodes.Plane024.geometry}
           material={materials['Material.066']}
         />
         <mesh
-          ref={ref2}
           name="Plane024_1"
           geometry={nodes.Plane024_1.geometry}
           material={materials['Material.067']}
         />
       </group>
     </group>
-  );
+  )
 }
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/oficina/pared2.glb');
