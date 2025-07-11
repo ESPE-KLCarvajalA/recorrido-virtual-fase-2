583af422bde097ad07bd7db809758cb520e19a4c
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-10 22:23:41 -0500

    cambios realizados 21.14

--- src/scenes/home/components/groups/BaseSceneArco.tsx
+++ src/scenes/home/components/groups/BaseSceneArco.tsx
@@ -3,7 +3,7 @@
 // Imports de tus componentes originales
 
 import { ParedE2 } from "../entrada2/ParedE2";
-import { TechoEntrada } from "../entrada2/TechoEntrada";
+// import { TechoEntrada } from "../entrada2/TechoEntrada";
 
 
 // cargar segun la distancia 
@@ -18,7 +18,7 @@ const BaseSceneArco = () => {
       <ParedE2 />
 
  {/* sin fisica */}
-      <TechoEntrada />
+      {/* <TechoEntrada /> */}
        
      
   
