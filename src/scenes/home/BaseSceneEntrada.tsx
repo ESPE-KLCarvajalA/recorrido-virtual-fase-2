318c0d6f65bda5fb9642b672e8799fedec17a4fb
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-13 20:35:04 -0500

    cambios realizados 21.69

--- src/scenes/home/BaseSceneEntrada.tsx
+++ src/scenes/home/BaseSceneEntrada.tsx
@@ -14,7 +14,7 @@ import BaseSceneAfuera from './components/groups/BaseSceneAfuera';
 import BaseSceneArco from './components/groups/BaseSceneArco';
 import BaseSceneBar from './components/groups/BaseSceneBar';
 // import BaseSceneOficina from './components/groups/BaseSceneOficina';
-import BaseSceneVilla from './components/groups/BaseSceneVilla';
+// import BaseSceneVilla from './components/groups/BaseSceneVilla';
 
 
 
@@ -38,7 +38,7 @@ function SceneContent() {
       <BaseSceneArco />
       <BaseSceneBar />
       {/* <BaseSceneOficina /> */}
-      <BaseSceneVilla />
+      {/* <BaseSceneVilla /> */}
 
       
     </>
