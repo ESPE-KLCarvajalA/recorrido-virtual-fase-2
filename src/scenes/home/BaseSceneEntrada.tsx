2340ef6b9cd1a139e7ccace0d100fdfdb4c33688
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-14 01:32:50 -0500

    cambios realizados 21.88

--- src/scenes/home/BaseSceneEntrada.tsx
+++ src/scenes/home/BaseSceneEntrada.tsx
@@ -3,80 +3,50 @@ import { Canvas } from '@react-three/fiber';
 import BaseCharacter from '../../shared/components/BaseCharacter';
 import { PointerLockControls } from '@react-three/drei';
 import { useRef } from 'react';
+import { PerformanceMonitor } from '../../shared/components/PerformanceMonitor'; // 👈 NUEVO
 
 import HDRIEnvironment from './components/ui/HDRIEnvironment';
 import BaseSceneAfuera from './groups/BaseSceneAfuera';
 import BaseSceneArco from './groups/BaseSceneArco';
-// import BaseSceneBar from './groups/BaseSceneBar';
-// import BaseSceneBar2 from './groups/BaseSceneBar2';
 import BaseSceneLab from './groups/BaseSceneLab';
 import BaseSceneLab2 from './groups/BaseSceneLab2';
 import BaseSceneOficina from './groups/BaseSceneOficina';
-// import BaseSceneVilla from './groups/BaseSceneVilla';
-// import BaseSceneVilla2 from './groups/BaseSceneVilla2';
 import BaseSceneOtros from './groups/BaseSceneOtros';
-
-import { PerformanceMonitor } from '../../shared/components/PerformanceMonitor';
-
-
-
 
 const BaseSceneEntrada = () => {
   const controlsRef = useRef(null);
 
   return (
     <>
-    <Canvas camera={{ position: [-92, 0, 29] }}>
+      <Canvas camera={{ position: [-80, 0, 29] }}>
+        <ambientLight intensity={Math.PI / 2} />
 
-      <ambientLight intensity={Math.PI / 2} />
+        <Physics gravity={[0, -100, 0]} iterations={10}>
+          <BaseSceneAfuera />
+          <BaseSceneArco />
+          <BaseSceneLab />
+          <BaseSceneLab2 />
+          <BaseSceneOficina />
+          <BaseSceneOtros />
 
+          <BaseCharacter 
+            controls 
+            positionCharacter={[-80,-1, 170]} 
+            args={[2.2]} 
+            altura={20} 
+            velocidad={40} 
+            salto={20} 
+            color="green" 
+          />
+        </Physics>
 
-      <Physics gravity={[0, -100, 0]} iterations={10}>
+        <HDRIEnvironment />
+        <PointerLockControls ref={controlsRef} />
+      </Canvas>
 
-
-        <BaseSceneAfuera />
-        <BaseSceneArco />
-        {/* <BaseSceneBar />
-        <BaseSceneBar2 /> */}
-        <BaseSceneLab />
-        <BaseSceneLab2 />
-        <BaseSceneOficina />
-        {/* <BaseSceneVilla />
-        <BaseSceneVilla2 /> */}
-        <BaseSceneOtros />
-
-        
-
-
-        <BaseCharacter controls positionCharacter={[-92,-1, 170]} args={[2.2]} altura={20} velocidad={40} salto={20} color="green" />
-
-        
-      </Physics>
-
-        
-
-      {/* sin fisicas */}
-
-    
-      <HDRIEnvironment />
-
-
-
-
-
-      <PointerLockControls ref={controlsRef} />
-
-    </Canvas>
-
-      <PerformanceMonitor />
-      </>
+      <PerformanceMonitor /> {/* 👈 NUEVO */}
+    </>
   );
 };
 
-
-
-
 export default BaseSceneEntrada;
-
-
-
