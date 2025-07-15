dd9a2a4af4603ef92f35bbb5797f63f16a8f0d56
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-15 04:21:19 -0500

    cambios realizados 21.114

--- src/scenes/home/components/pisos/PisoPrueba.tsx
+++ src/scenes/home/components/pisos/PisoPrueba.tsx
@@ -2,7 +2,6 @@ import * as THREE from 'three'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
 import { ThreeElements } from '@react-three/fiber'
-import { useTrimesh } from '@react-three/cannon'
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -14,32 +13,19 @@ type GLTFResult = GLTF & {
     ['Material.061']: THREE.MeshStandardMaterial
   }
 }
-
 export function PisoPrueba(props: ThreeElements['group']) {
   const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/pisos/pisoprueba.glb') as unknown as GLTFResult
-
-  const [ref1] = useTrimesh(() => ({
-    type: 'Static',
-    args: [nodes.road009.geometry.attributes.position.array, nodes.road009.geometry.index.array],
-    position: [-65.404, -1.118, -457.82],
-  }))
-
-  const [ref2] = useTrimesh(() => ({
-    type: 'Static',
-    args: [nodes.road016.geometry.attributes.position.array, nodes.road016.geometry.index.array],
-    position: [-100.871, -0.481, -796.902],
-  }))
 
   return (
     <group {...props} dispose={null}>
       <mesh
-        ref={ref1}
+        name="road009"
         geometry={nodes.road009.geometry}
         material={materials['Material.048']}
         position={[-65.404, -1.118, -457.82]}
       />
       <mesh
-        ref={ref2}
+        name="road016"
         geometry={nodes.road016.geometry}
         material={materials['Material.061']}
         position={[-100.871, -0.481, -796.902]}
@@ -47,5 +33,6 @@ export function PisoPrueba(props: ThreeElements['group']) {
     </group>
   )
 }
+  
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/pisos/pisoprueba.glb')
