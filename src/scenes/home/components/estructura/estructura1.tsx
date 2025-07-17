ca404ed8c18896e5acf98f6a3bfa6f6fd73040aa
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-17 09:31:26 -0500

    cambios realizados 21.137

--- src/scenes/home/components/estructura/estructura1.tsx
+++ src/scenes/home/components/estructura/estructura1.tsx
@@ -1,11 +1,11 @@
+
 import * as THREE from 'three'
-import { ThreeElements } from '@react-three/fiber'
-import { useGLTF, Instances, Instance } from '@react-three/drei'
+import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
+import { ThreeElements } from '@react-three/fiber'
 
 type GLTFResult = GLTF & {
   nodes: {
-    Cube069: THREE.Mesh
     Cube070: THREE.Mesh
   }
   materials: {
@@ -20,25 +20,16 @@ export function Estructura1(props: ThreeElements['group']) {
 
   return (
     <group {...props} dispose={null}>
-      <Instances
-        geometry={nodes.Cube069.geometry}
+      <mesh
+        name="Cube070"
+        geometry={nodes.Cube070.geometry}
         material={materials['Material.051']}
-      >
-        <Instance
-          name="Cube069"
-          position={[-280.251, 103, 723]}
-          scale={[1, 0.7, 1]}
-        />
-        <Instance
-          name="Cube070"
-          position={[-173.318, 144.23, 648.364]}
-          rotation={[0, -0.018, 0]}
-        />
-      </Instances>
+        position={[-173.318, 144.23, 648.364]}
+        rotation={[0, -0.018, 0]}
+      />
     </group>
   )
 }
 
-useGLTF.preload(
-  'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb'
-)
+
+useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb')
