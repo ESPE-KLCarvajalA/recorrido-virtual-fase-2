053ec6a5804cf07750a81bd059e33f3ef08d6d85
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-17 01:38:37 -0500

    cambios realizados 21.125

--- src/scenes/home/components/estructura/estructura.tsx
+++ src/scenes/home/components/estructura/estructura.tsx
@@ -7,16 +7,12 @@ import { GLTF } from 'three-stdlib';
 
 type GLTFResult = GLTF & {
     nodes: {
-        Plane056: THREE.Mesh;
-        Plane056_1: THREE.Mesh;
-        Plane056_2: THREE.Mesh;
-    };
+        forma050: THREE.Mesh
+    }
     materials: {
-        ['verde.002']: THREE.MeshStandardMaterial;
-        ['Material.045']: THREE.MeshStandardMaterial;
-        ['White Glazed Bricks.001']: THREE.MeshStandardMaterial;
-    };
-};
+        ['Material.045']: THREE.MeshStandardMaterial
+    }
+}
 
 type Instance = {
     position: [number, number, number];
@@ -24,7 +20,7 @@ type Instance = {
     scale: [number, number, number];
 };
 
-// 👇 Reemplaza este array por el tuyo completo
+
 const instances: Instance[] = [
     { position: [-58.822, 23, 553.073], rotation: [0, 0, 0], scale: [1, 1, 1] },
     { position: [-18.424, 23, 517.935], rotation: [0, 0, 0], scale: [1, 1, 1] },
@@ -60,18 +56,17 @@ const instances: Instance[] = [
     { position: [616.395, 21.32, -456.814], rotation: [0, 1.2, 0], scale: [1, 1, 1] },
 
 
-
     { position: [-322.682, 15.372, 785.385], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-364.275, 15.372, 820.301], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-405.855, 15.372, 855.401], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
-    { position: [-446.252, 15.372, 890.929], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-486.129, 15.372, 927.492], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-526.064, 15.372, 963.641], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-566.031, 15.372, 1000.092], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-605.773, 15.372, 1036.352], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-645.571, 15.372, 1072.611], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-685.388, 15.372, 1108.973], rotation: [0,3.1, 0], scale: [1, 1, 1] },
-    { position: [-724.951, 15.372, 1145.265], rotation: [0,3.1, 0], scale: [1, 1, 1] },
+    { position: [-446.252, 15.372, 890.929], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-486.129, 15.372, 927.492], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-526.064, 15.372, 963.641], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-566.031, 15.372, 1000.092], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-605.773, 15.372, 1036.352], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-645.571, 15.372, 1072.611], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-685.388, 15.372, 1108.973], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
+    { position: [-724.951, 15.372, 1145.265], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-764.7, 15.372, 1181.781], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-804.214, 15.372, 1217.98], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-844.095, 15.372, 1254.766], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
@@ -79,80 +74,52 @@ const instances: Instance[] = [
     { position: [-923.58, 15.372, 1327.348], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-963.321, 15.372, 1363.789], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
     { position: [-1003.192, 15.372, 1400.079], rotation: [0, 3.1, 0], scale: [1, 1, 1] },
-    {
-        position: [-100, 23, 590],
-        rotation: [0, 0, 0],
-        scale: [1, 1, 1]
-      },
-      {
-        position: [-140, 23, 627],
-        rotation: [0, 0, 0],
-        scale: [1, 1, 1]
-      }
-
-
-
-
+    { position: [-100, 23, 590], rotation: [0, 0, 0], scale: [1, 1, 1]},
+    { position: [-140, 23, 627], rotation: [0, 0, 0], scale: [1, 1, 1]}
 ];
 
 
-
 export function Estructura() {
-    const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura.glb') as unknown as GLTFResult;
+    const { nodes, materials } = useGLTF(
+        'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura.glb'
+    ) as unknown as GLTFResult;
 
-    const ref1 = useRef<THREE.InstancedMesh>(null);
-    const ref2 = useRef<THREE.InstancedMesh>(null);
-    const ref3 = useRef<THREE.InstancedMesh>(null);
+    const ref = useRef<THREE.InstancedMesh>(null);
 
     useEffect(() => {
+        if (!ref.current) return;
+
+        const dummyMatrix = new THREE.Matrix4();
+        const quaternion = new THREE.Quaternion();
+
         instances.forEach((inst, i) => {
-            const matrix = new THREE.Matrix4();
             const position = new THREE.Vector3(...inst.position);
             const rotation = new THREE.Euler(...inst.rotation);
             const scale = new THREE.Vector3(...inst.scale);
-            matrix.compose(position, new THREE.Quaternion().setFromEuler(rotation), scale);
 
-            ref1.current?.setMatrixAt(i, matrix);
-            ref2.current?.setMatrixAt(i, matrix);
-            ref3.current?.setMatrixAt(i, matrix);
+            quaternion.setFromEuler(rotation);
+            dummyMatrix.compose(position, quaternion, scale);
+
+            ref.current!.setMatrixAt(i, dummyMatrix);
         });
 
-        if (ref1.current) {
-            ref1.current.frustumCulled = false;
-            ref1.current.instanceMatrix.needsUpdate = true;
-        }
-        if (ref2.current) {
-            ref2.current.frustumCulled = false;
-            ref2.current.instanceMatrix.needsUpdate = true;
-        }
-        if (ref3.current) {
-            ref3.current.frustumCulled = false;
-            ref3.current.instanceMatrix.needsUpdate = true;
-        }
+        ref.current.instanceMatrix.needsUpdate = true;
+        ref.current.frustumCulled = false;
     }, []);
 
     return (
-        <group>
+        <group dispose={null}>
             <instancedMesh
-                ref={ref1}
-                args={[undefined, undefined, instances.length]}
-                geometry={nodes.Plane056.geometry}
-                material={materials['verde.002']}
-            />
-            <instancedMesh
-                ref={ref2}
-                args={[undefined, undefined, instances.length]}
-                geometry={nodes.Plane056_1.geometry}
-                material={materials['Material.045']}
-            />
-            <instancedMesh
-                ref={ref3}
-                args={[undefined, undefined, instances.length]}
-                geometry={nodes.Plane056_2.geometry}
-                material={materials['White Glazed Bricks.001']}
-            />
+                ref={ref}
+                args={[null, null, instances.length]} // Número de instancias
+            >
+                <bufferGeometry attach="geometry" {...nodes.forma050.geometry} />
+                <meshStandardMaterial attach="material" {...materials['Material.045']} />
+            </instancedMesh>
         </group>
     );
 }
 
-useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura.glb');
+useGLTF.preload(
+    'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura.glb'
+);
--- src/scenes/home/groups/BaseSceneOtros2.tsx
+++ src/scenes/home/groups/BaseSceneOtros2.tsx
@@ -13,6 +13,7 @@ const BaseSceneOtros = () => {
     <Pizarron />
     <Compus />
     
+    
      
   </>
   );
