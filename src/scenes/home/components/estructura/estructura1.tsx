9cfd8a8cdf940562e0d0f05f4d2f0efd152418de
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-17 02:25:16 -0500

    cambios realizados 21.126

--- src/scenes/home/components/estructura/estructura1.tsx
+++ src/scenes/home/components/estructura/estructura1.tsx
@@ -1,42 +1,44 @@
 import * as THREE from 'three'
 import { ThreeElements } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, Instances, Instance } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
-
-
 
 type GLTFResult = GLTF & {
   nodes: {
-    Cube070: THREE.Mesh
     Cube069: THREE.Mesh
+    Cube070: THREE.Mesh
   }
   materials: {
     ['Material.051']: THREE.MeshStandardMaterial
   }
 }
 
-
 export function Estructura1(props: ThreeElements['group']) {
-  const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb') as unknown as GLTFResult
+  const { nodes, materials } = useGLTF(
+    'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb'
+  ) as unknown as GLTFResult
+
   return (
     <group {...props} dispose={null}>
-      <mesh
-        name="Cube070"
-        geometry={nodes.Cube070.geometry}
-        material={materials['Material.051']}
-        position={[-173.318, 104, 648.364]}
-        rotation={[0, 0.2, 0]}
-        scale={[1,0.7,1]}
-      />
-      <mesh
-        name="Cube069"
+      <Instances
         geometry={nodes.Cube069.geometry}
         material={materials['Material.051']}
-        position={[-280.251, 103, 723]}
-        scale={[1,0.7,1]}
-      />
+      >
+        <Instance
+          name="Cube069"
+          position={[-280.251, 103, 723]}
+          scale={[1, 0.7, 1]}
+        />
+        <Instance
+          name="Cube070"
+          position={[-173.318, 144.23, 648.364]}
+          rotation={[0, -0.018, 0]}
+        />
+      </Instances>
     </group>
   )
 }
 
-useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb')
+useGLTF.preload(
+  'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/estructura/estructura1.glb'
+)
