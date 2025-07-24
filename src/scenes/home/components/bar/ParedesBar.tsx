db8e0bbbb7a5c2a12a401740b7d28ef8789a05c2
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-24 00:11:04 -0500

    cambios realizados 21.165

--- src/scenes/home/components/bar/ParedesBar.tsx
+++ src/scenes/home/components/bar/ParedesBar.tsx
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
 import { ThreeElements } from '@react-three/fiber'
-import { useConvexPolyhedron } from '@react-three/cannon'
+
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -15,43 +15,17 @@ type GLTFResult = GLTF & {
   }
 }
 
-// Extrae vértices y caras de una geometría no indexada
-function getConvexPolyhedronArgs(geometry: THREE.BufferGeometry) {
-  geometry.computeVertexNormals()
-  geometry = geometry.toNonIndexed()
-
-  const positions = geometry.attributes.position.array as Float32Array
-  const vertices: THREE.Vector3[] = []
-
-  for (let i = 0; i < positions.length; i += 3) {
-    vertices.push(new THREE.Vector3(positions[i], positions[i + 1], positions[i + 2]))
-  }
-
-  const faces: number[][] = []
-  for (let i = 0; i < vertices.length; i += 3) {
-    faces.push([i, i + 1, i + 2])
-  }
-
-  return [vertices, faces] as [THREE.Vector3[], number[][]]
-}
 
 export function ParedesBar(props: ThreeElements['group']) {
   const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/bar/paredesBar.glb') as unknown as GLTFResult
 
-  const position: [number, number, number] = [-854.077, -9.046, -291.626]
-  const rotation: [number, number, number] = [0, -Math.PI / 2, 0]
-  const scale: [number, number, number] = [1.051, 31.523, 2.999]
-
-  const [ref] = useConvexPolyhedron(() => ({
-    args: getConvexPolyhedronArgs(nodes.Room027.geometry),
-    position,
-    rotation,
-    type: 'Static',
-  }))
-
   return (
     <group {...props} dispose={null}>
-      <group name="Room084" position={position} rotation={rotation} scale={scale} ref={ref}>
+      <group
+        name="Room084"
+        position={[-854.077, -9.046, -291.626]}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={[1.051, 31.523, 2.999]}>
         <mesh
           name="Room027"
           geometry={nodes.Room027.geometry}
@@ -66,5 +40,6 @@ export function ParedesBar(props: ThreeElements['group']) {
     </group>
   )
 }
+
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/bar/paredesBar.glb')
