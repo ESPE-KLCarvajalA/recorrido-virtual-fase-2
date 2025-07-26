5d870f4360a125e7bb29d52bc09c61096d7d6271
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-26 15:16:19 -0500

    cambios realizados 21.197

--- src/scenes/home/components/ui/HDRIEnvironment.tsx
+++ src/scenes/home/components/ui/HDRIEnvironment.tsx
@@ -14,7 +14,6 @@ const HDRIEnvironment = () => {
       <Environment
         background={true}
         files="https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/hdr/pretoria_gardens_1k.hdr"
-        backgroundBlurriness={0.06}
         environmentIntensity={0.4}
         backgroundIntensity={1.5}
         environmentRotation={[-0.2, 0.15, -0.1]}
