8d870d3e08f5bdb82e38206e584a7d1af58ced6b
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-07-26 00:47:14 -0500

    cambios realizados 21.175

--- src/scenes/home/BaseSceneEntrada.tsx
+++ src/scenes/home/BaseSceneEntrada.tsx
@@ -5,16 +5,16 @@ import { PointerLockControls } from '@react-three/drei';
 import { useRef } from 'react';
 
 import HDRIEnvironment from './components/ui/HDRIEnvironment';
-import BaseSceneAfuera from './groups/BaseSceneAfuera';
-import BaseSceneArco from './groups/BaseSceneArco';
-import BaseSceneLab from './groups/BaseSceneLab';
-import BaseSceneLab2 from './groups/BaseSceneLab2';
-import BaseSceneOficina from './groups/BaseSceneOficina';
-import BaseSceneOtros from './groups/BaseSceneOtros';
-import BaseScenePisos2 from './groups/BaseScenePisos2';
-import BaseSceneBar from './groups/BaseSceneBar';
-import BaseSceneBar2 from './groups/BaseSceneBar2';
-import BaseSceneVilla from './groups/BaseSceneVilla';
+// import BaseSceneAfuera from './groups/BaseSceneAfuera';
+// import BaseSceneArco from './groups/BaseSceneArco';
+// import BaseSceneLab from './groups/BaseSceneLab';
+// import BaseSceneLab2 from './groups/BaseSceneLab2';
+// import BaseSceneOficina from './groups/BaseSceneOficina';
+// import BaseSceneOtros from './groups/BaseSceneOtros';
+// import BaseScenePisos2 from './groups/BaseScenePisos2';
+// import BaseSceneBar from './groups/BaseSceneBar';
+// import BaseSceneBar2 from './groups/BaseSceneBar2';
+// import BaseSceneVilla from './groups/BaseSceneVilla';
 import BaseSceneVilla2 from './groups/BaseSceneVilla2';
 
 const BaseSceneEntrada = () => {
@@ -26,6 +26,7 @@ const BaseSceneEntrada = () => {
         <ambientLight intensity={Math.PI / 2} />
 
         <Physics gravity={[0, -100, 0]} iterations={10}>
+         {/* 
           <BaseSceneAfuera />
           <BaseSceneArco />
           <BaseSceneLab />
@@ -36,9 +37,8 @@ const BaseSceneEntrada = () => {
           <BaseSceneBar />
           <BaseSceneBar2 />
           <BaseSceneVilla />
-          <BaseSceneVilla2 /> 
-         {/* 
           */}
+          <BaseSceneVilla2 /> 
 
 
 
