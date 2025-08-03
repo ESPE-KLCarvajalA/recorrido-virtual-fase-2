a763d16f595e42b16260d902cdf8a9a8a3644ced
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-08-02 19:59:29 -0500

    cambios realizados 21.264

--- src/scenes/home/components/sillas/sillasLab.tsx
+++ src/scenes/home/components/sillas/sillasLab.tsx
@@ -27,12 +27,14 @@ export function SillasLab() {
   const ref2 = useRef<THREE.InstancedMesh>(null);
 
   const instances: InstanceData[] = [
-    { position: [19.402, 10, -335.884], rotation: [0, 0, 0], scale: [1, 1, 1] },
-    { position: [157.668, 10, -299.409], rotation: [0, Math.PI , 0], scale: [1, 1, 1] },
-    { position: [-21.618, 10, -167.891], rotation: [0, Math.PI / 2, 0], scale: [1, 1, 1] },
-    { position: [-21.618, 10, -203.551], rotation: [0, Math.PI / 2, 0], scale: [1, 1, 1] },
-    { position: [71.222, 10, -137.708], rotation: [0, Math.PI, 0], scale: [1, 1, 1] },
-    { position: [35.563, 10, -137.708], rotation: [0, Math.PI, 0], scale: [1, 1, 1] },
+    { position: [19.402, 9, -335.884], rotation: [0, 0, 0], scale: [1, 1, 1] },
+    { position: [157.668, 9, -299.409], rotation: [0, -Math.PI / 2, 0], scale: [1, 1, 1] },
+    
+    { position: [-21.618, 9, -167.891], rotation: [0, Math.PI / 2, 0], scale: [1, 1, 1] },
+    { position: [-21.618, 9, -203.551], rotation: [0, Math.PI / 2, 0], scale: [1, 1, 1] },
+    
+    { position: [71, 9, -137.708], rotation: [0, Math.PI, 0], scale: [1, 1, 1] },
+    { position: [35, 9, -137.708], rotation: [0, Math.PI, 0], scale: [1, 1, 1] },
 
 
     // Puedes agregar más aquí...
