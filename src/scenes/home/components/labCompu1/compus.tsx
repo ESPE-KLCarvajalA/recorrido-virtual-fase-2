0b70e4b0789bec245f61875764cd8a6092081cc6
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-08-03 11:36:42 -0500

    cambios realizados 21.249

--- src/scenes/home/components/labCompu1/compus.tsx
+++ src/scenes/home/components/labCompu1/compus.tsx
@@ -1,52 +1,103 @@
 import * as THREE from 'three';
 import { useGLTF } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
 import { GLTF } from 'three-stdlib';
-import { ThreeElements } from '@react-three/fiber';
 
+// Tipo del modelo GLB
 type GLTFResult = GLTF & {
   nodes: {
-    Cube078: THREE.Mesh
-    Cube078_1: THREE.Mesh
-    Cube078_2: THREE.Mesh
-    Cube078_3: THREE.Mesh
-  }
+    Cube078: THREE.Mesh;
+    Cube078_1: THREE.Mesh;
+    Cube078_2: THREE.Mesh;
+    Cube078_3: THREE.Mesh;
+  };
   materials: {
-    ['black.005']: THREE.MeshStandardMaterial
-    ['Material.200']: THREE.MeshStandardMaterial
-    ['Material.011']: THREE.MeshStandardMaterial
-    ['Material.012']: THREE.MeshStandardMaterial
-  }
-}
+    ['black.005']: THREE.MeshStandardMaterial;
+    ['Material.200']: THREE.MeshStandardMaterial;
+    ['Material.011']: THREE.MeshStandardMaterial;
+    ['Material.012']: THREE.MeshStandardMaterial;
+  };
+};
 
-export function Compus(props: ThreeElements['group']) {
+type InstanceData = {
+  position: [number, number, number];
+  rotation: [number, number, number];
+  scale: [number, number, number];
+};
+
+export function Compus() {
   const { nodes, materials } = useGLTF(
     'https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/labCom1/computadora.glb'
   ) as unknown as GLTFResult;
 
-  return (
-    <group {...props} dispose={null}>
-      <group name="Moniter023" position={[163.33, 13.208, -23.434]}>
-        <mesh name="Cube078" geometry={nodes.Cube078.geometry} material={materials['black.005']} />
-        <mesh
-          name="Cube078_1"
-          geometry={nodes.Cube078_1.geometry}
-          material={materials['Material.200']}
-        />
-        <mesh
-          name="Cube078_2"
-          geometry={nodes.Cube078_2.geometry}
-          material={materials['Material.011']}
-        />
-        <mesh
-          name="Cube078_3"
-          geometry={nodes.Cube078_3.geometry}
-          material={materials['Material.012']}
-        />
-      </group>
-    </group>
-  )
-}
+  const Cube078 = useRef<THREE.InstancedMesh>(null);
+  const Cube078_1 = useRef<THREE.InstancedMesh>(null);
+  const Cube078_2 = useRef<THREE.InstancedMesh>(null);
+  const Cube078_3 = useRef<THREE.InstancedMesh>(null);
 
-  
+  const instances: InstanceData[] = [
+    { position: [163.33, 13.208, -23.434], rotation: [0, 0, 0], scale: [1, 1, 1] },
+   
+
+{ position: [160.675, 16.884, -52.474], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [160.675, 16.884, -81.409], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [160.675, 16.884, -111.033], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [189.58, 16.884, -111.033], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [189.58, 16.884, -81.409], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [189.58, 16.884, -52.474], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [189.58, 16.884, -23.539], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [218.572, 16.884, -23.539], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [218.572, 16.884, -52.474], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [218.572, 16.884, -81.409], rotation: [0, 0, 0], scale: [1, 1, 1] },
+{ position: [218.572, 16.884, -111.033], rotation: [0, 0, 0], scale: [1, 1, 1] },
+
+  ];
+
+  useEffect(() => {
+    instances.forEach((instance, i) => {
+      const position = new THREE.Vector3(...instance.position);
+      const rotation = new THREE.Euler(...instance.rotation);
+      const scale = new THREE.Vector3(...instance.scale);
+      const matrix = new THREE.Matrix4();
+      matrix.compose(position, new THREE.Quaternion().setFromEuler(rotation), scale);
+
+      Cube078.current!.setMatrixAt(i, matrix);
+      Cube078_1.current!.setMatrixAt(i, matrix);
+      Cube078_2.current!.setMatrixAt(i, matrix);
+      Cube078_3.current!.setMatrixAt(i, matrix);
+
+      Cube078.current!.frustumCulled = false;
+      Cube078_1.current!.frustumCulled = false;
+      Cube078_2.current!.frustumCulled = false;
+      Cube078_3.current!.frustumCulled = false;
+    });
+
+    Cube078.current!.instanceMatrix.needsUpdate = true;
+    Cube078_1.current!.instanceMatrix.needsUpdate = true;
+    Cube078_2.current!.instanceMatrix.needsUpdate = true;
+    Cube078_3.current!.instanceMatrix.needsUpdate = true;
+  }, [instances]);
+
+  return (
+    <group>
+      <instancedMesh ref={Cube078} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube078.geometry} />
+        <meshStandardMaterial attach="material" {...materials['black.005']} />
+      </instancedMesh>
+      <instancedMesh ref={Cube078_1} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube078_1.geometry} />
+        <meshStandardMaterial attach="material" {...materials['Material.200']} />
+      </instancedMesh>
+      <instancedMesh ref={Cube078_2} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube078_2.geometry} />
+        <meshStandardMaterial attach="material" {...materials['Material.011']} />
+      </instancedMesh>
+      <instancedMesh ref={Cube078_3} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube078_3.geometry} />
+        <meshStandardMaterial attach="material" {...materials['Material.012']} />
+      </instancedMesh>
+    </group>
+  );
+}
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/labCom1/computadora.glb');
