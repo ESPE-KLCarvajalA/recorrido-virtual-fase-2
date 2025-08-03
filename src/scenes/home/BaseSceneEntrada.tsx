02144ee1974632de72e99839d546fe28912005d4
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-08-02 20:24:04 -0500

    cambios realizados 21.264

--- src/scenes/home/BaseSceneEntrada.tsx
+++ src/scenes/home/BaseSceneEntrada.tsx
@@ -60,7 +60,7 @@ const BaseSceneEntrada = () => {
 
           <BaseCharacter 
             controls 
-            positionCharacter={[-80,-1, 170]} 
+            positionCharacter={[9.373, -4, -247.046]} 
             args={[2.2]} 
             altura={20} 
             velocidad={40} 
