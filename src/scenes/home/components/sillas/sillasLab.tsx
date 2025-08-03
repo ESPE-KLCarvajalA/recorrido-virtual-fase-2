edd2bd24f4bee82084a6db914cdfc6622ab3d535
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-08-02 19:04:59 -0500

    cambios realizados 21.261

--- src/scenes/home/components/sillas/sillasLab.tsx
+++ src/scenes/home/components/sillas/sillasLab.tsx
@@ -1,10 +1,7 @@
 import * as THREE from 'three';
 import { useGLTF } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
 import { GLTF } from 'three-stdlib';
-import { ThreeElements } from '@react-three/fiber';
-// import useCameraDistance from '../../../../utils/useCameraDistance'; // Ajusta la ruta si es necesario
-
-
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -17,26 +14,71 @@ type GLTFResult = GLTF & {
   }
 }
 
+type InstanceData = {
+  position: [number, number, number];
+  rotation: [number, number, number];
+  scale: [number, number, number];
+};
 
-export function SillasLab(props: ThreeElements['group']) {
+export function SillasLabInstanciadas() {
   const { nodes, materials } = useGLTF('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/sillas/sillalab1.glb') as unknown as GLTFResult;
 
+  const ref1 = useRef<THREE.InstancedMesh>(null);
+  const ref2 = useRef<THREE.InstancedMesh>(null);
+
+  const instances: InstanceData[] = [
+    { position: [19.402, 10.034, -335.884], rotation: [0, 0, 0], scale: [1, 1, 1] },
+    { position: [157.668, 11.64, -299.409], rotation: [0, Math.PI / 2, 0], scale: [1, 1, 1] },
+    { position: [-21.618, 11.768, -167.891], rotation: [0, -Math.PI / 2, 0], scale: [1, 1, 1] },
+    { position: [-21.618, 11.768, -203.551], rotation: [0, -Math.PI / 2, 0], scale: [1, 1, 1] },
+    { position: [71.222, 11.64, -137.708], rotation: [0, 0, 0], scale: [1, 1, 1] },
+    { position: [35.563, 11.64, -137.708], rotation: [0, 0, 0], scale: [1, 1, 1] },
+
+
+    // Puedes agregar más aquí...
+  ];
+
+  useEffect(() => {
+    instances.forEach((instance, i) => {
+      const pos = new THREE.Vector3(...instance.position);
+      const rot = new THREE.Euler(...instance.rotation);
+      const scl = new THREE.Vector3(...instance.scale);
+      const matrix = new THREE.Matrix4();
+      matrix.compose(pos, new THREE.Quaternion().setFromEuler(rot), scl);
+
+      ref1.current?.setMatrixAt(i, matrix);
+      ref2.current?.setMatrixAt(i, matrix);
+    });
+
+    ref1.current!.instanceMatrix.needsUpdate = true;
+    ref2.current!.instanceMatrix.needsUpdate = true;
+
+    ref1.current!.frustumCulled = false;
+    ref2.current!.frustumCulled = false;
+  }, [instances]);
+
   return (
-    <group {...props} dispose={null}>
-      <group name="Base" position={[19.23, 5.343, -333.243]}>
-        <mesh
-          name="Cube053"
-          geometry={nodes.Cube053.geometry}
-          material={materials['Material.216']}
-        />
-        <mesh
-          name="Cube053_1"
-          geometry={nodes.Cube053_1.geometry}
-          material={materials['Material.217']}
-        />
-      </group>
+    <group>
+      <instancedMesh ref={ref1} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube053.geometry} />
+        <meshStandardMaterial attach="material" {...materials['Material.216']} />
+      </instancedMesh>
+      <instancedMesh ref={ref2} args={[null, null, instances.length]}>
+        <bufferGeometry attach="geometry" {...nodes.Cube053_1.geometry} />
+        <meshStandardMaterial attach="material" {...materials['Material.217']} />
+      </instancedMesh>
     </group>
-  )
+  );
 }
 
 useGLTF.preload('https://pub-c5bac125f50b4d948ed14a01abf7fef0.r2.dev/models/sillas/sillalab1.glb');
+
+
+
+
+
+position: [83.618, 5.343, -139.896]
+position: [47.959, 5.343, -139.896]
+position: [-21.135, 5.343, -155.442]
+position: [-21.135, 5.343, -191.403]
+position: [155.947, 5.343, -311.672]
